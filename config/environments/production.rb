579801535cd0e16e469e162030f1f33708ffac00
Israel Valdez <kailer89@gmail.com>
2017-06-10 09:07:43 -0500

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ ProtoCRM3::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store, { size: 64.megabytes }
+  # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
