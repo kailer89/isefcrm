328cc0d177edfafd663888ef1220a05b1e95311c
isefcrm <israel.valdez.89@gmail.com>
2017-09-02 23:41:16 -0500

    fix fields

--- db/migrate/20170903043338_assd.rb
+++ db/migrate/20170903043338_assd.rb
@@ -1,9 +1,9 @@
 class Assd < ActiveRecord::Migration
    def up
     add_column :configuraciones, :LastSearchProspecto, :text
-    add_column :Configuraciones, :LastSearchSoliticante, :text
-    add_column :Configuraciones, :LastSearchExaminado, :text
-    add_column :Configuraciones, :LastSearchAdmitido, :text
-    add_column :Configuraciones, :LastSearchInscrito, :text
+    add_column :configuraciones, :LastSearchSoliticante, :text
+    add_column :configuraciones, :LastSearchExaminado, :text
+    add_column :configuraciones, :LastSearchAdmitido, :text
+    add_column :configuraciones, :LastSearchInscrito, :text
   end
 end
